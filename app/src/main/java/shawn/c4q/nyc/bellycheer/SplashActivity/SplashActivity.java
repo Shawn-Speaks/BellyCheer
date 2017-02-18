68617e5f50bd05bab6ea5e1000beca6b4bfec452
catcattac <catwong.nyc@gmail.com>
2017-02-18 14:02:21 -0500

    added animation

--- app/src/main/java/shawn/c4q/nyc/bellycheer/SplashActivity/SplashActivity.java
+++ app/src/main/java/shawn/c4q/nyc/bellycheer/SplashActivity/SplashActivity.java
@@ -1,15 +1,46 @@
 package shawn.c4q.nyc.bellycheer.SplashActivity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
+import shawn.c4q.nyc.bellycheer.MainActivity;
 import shawn.c4q.nyc.bellycheer.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Thread splashThread;
+    ImageView iv_foods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setAnimation();
+
+
     }
+
+    public void setAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.anim_move_up);
+        ImageView iv = (ImageView) findViewById(R.id.iv_bellycheer_foods);
+        iv.setAnimation(animation);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 6000);
+
+    }
+
+
 }
+
