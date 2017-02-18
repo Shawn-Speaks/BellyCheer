069a94fcad0718017397b57758bad308046649d0
shawn-speaks <sspeaks121@gmail.com>
2017-02-18 13:36:38 -0500

    Working retrofit to Zipcode JSON server

---
+++ app/src/main/java/shawn/c4q/nyc/bellycheer/shawn/smodels/ZipObject.java
@@ -0,0 +1,11 @@
+package shawn.c4q.nyc.bellycheer.shawn.smodels;
+
+/**
+ * Created by shawnspeaks on 2/18/17.
+ */
+
+public class ZipObject {
+
+    private String postalCode;
+
+}
---
+++ app/src/main/java/shawn/c4q/nyc/bellycheer/shawn/smodels/ZipResponse.java
@@ -0,0 +1,13 @@
+package shawn.c4q.nyc.bellycheer.shawn.smodels;
+
+import java.util.List;
+
+/**
+ * Created by shawnspeaks on 2/18/17.
+ */
+
+public class ZipResponse {
+
+    private List<ZipObject> zipObjectList;
+
+}
