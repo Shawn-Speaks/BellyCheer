421a586272bf55f78d5787730738c2a3b51dc3d7
Jose Vila <you@example.comJDVila@gmail.com>
2017-02-19 03:12:55 -0500

    added query and cardview and zipcode intent

--- app/src/main/java/shawn/c4q/nyc/bellycheer/SearchScreenActivity.java
+++ app/src/main/java/shawn/c4q/nyc/bellycheer/SearchScreenActivity.java
@@ -1,8 +1,12 @@
 package shawn.c4q.nyc.bellycheer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by yojanasharma on 2/18/17.
@@ -11,14 +15,39 @@ import android.widget.ImageButton;
 public class SearchScreenActivity extends AppCompatActivity {
     private String zipCode;
     private String siteType;
-    ImageButton foodPantry;
-    ImageButton soupKitchen;
-    ImageButton bothSites;
+    private EditText zipEditText;
+    private ImageButton currentLoc;
+    private ImageButton foodPantry;
+    private ImageButton soupKitchen;
+    private ImageButton bothSites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_screen_activity);
+        zipEditText = (EditText) findViewById(R.id.editext_zipcode);
+        currentLoc = (ImageButton) findViewById(R.id.imagebutton_googlemap);
+        bothSites = (ImageButton) findViewById(R.id.submit_button);
 
+        currentLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zipEditText.setText("11101");
+            }
+        });
+
+        bothSites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (zipEditText.getText().toString().equals("")) {
+                    Toast.makeText(SearchScreenActivity.this, "Please Enter Zip Code", Toast.LENGTH_SHORT).show();
+                } else {
+                    zipCode = zipEditText.getText().toString();
+                    Intent intent = new Intent(SearchScreenActivity.this, PantryRecyclerViewActivity.class);
+                    intent.putExtra("zipCode", zipCode);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
