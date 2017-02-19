3b17ee4dbed6d23b00288053f3efa41ec4a99ed4
Jose Vila <you@example.comJDVila@gmail.com>
2017-02-18 23:14:29 -0500

    location activity updates

--- app/src/main/java/shawn/c4q/nyc/bellycheer/SearchScreenActivity.java
+++ app/src/main/java/shawn/c4q/nyc/bellycheer/SearchScreenActivity.java
@@ -2,15 +2,23 @@ package shawn.c4q.nyc.bellycheer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
 
 /**
  * Created by yojanasharma on 2/18/17.
  */
 
 public class SearchScreenActivity extends AppCompatActivity {
+    private String zipCode;
+    private String siteType;
+    ImageButton foodPantry;
+    ImageButton soupKitchen;
+    ImageButton bothSites;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_screen_activity);
+
     }
 }
